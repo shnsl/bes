57ac2ee655ce216c3672eb6f1e2e622fc0ba17f1
shnsl <omerhilalpt@gmail.com>
2026-09-25 19:20:09 +0300

    Add maximum file size limit for caching in Workbox configuration

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,otf,ttf,woff2}"],
         navigateFallback: "index.html",
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
     }),
   ],
